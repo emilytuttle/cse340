2263b3ee09aebe4826aee51cd8a2ddaefca3a34a
Emily Tuttle <emilytuttle00@gmail.com>
2024-12-10 02:32:42 -0700

    error fix

--- routes/errorRoute.js
+++
@@ -1,8 +0,0 @@
-// Needed Resources 
-const express = require("express")
-const router = new express.Router() 
-const errorController = require("../controllers/errorController")
-
-router.get("/error", errorController.buildError)
-
-module.exports = router
